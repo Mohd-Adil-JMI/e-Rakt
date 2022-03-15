d4cd7c59d631b86604857f973fcc19591a366b34
Soban Farooq <sobanfarooq911@gmail.com>
2022-03-15 11:55:44 +0530

    updated env file and some minor changes

--- db/database.js
+++ db/database.js
@@ -1,12 +1,14 @@
 const mysql = require('mysql2');
+const dotenv = require('dotenv');
+dotenv.config({ path: './.env' });
 
 var pool = mysql.createPool({
-    connectionLimit : 10,
-    host     : 'localhost',
-    user     : 'root',
-    password : 'Soban@123',
-    database : 'e_rakt',
-    debug    :  false
+    connectionLimit: 10,
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE,
+    debug: false
 });
 
 module.exports = pool;
