dfc6c5e46ddc0975fa37070d8691a94a9ae60368
Mohd Adil <aadil97606@gmail.com>
2022-03-20 12:19:42 +0530

    table query

--- db/database.js
+++ db/database.js
@@ -10,5 +10,22 @@ var pool = mysql.createPool({
     database: process.env.DATABASE,
     debug: false
 });
-
+const userSchema = 'create table if not exists users (user_id int primary key auto_increment,First_Name varchar(20),Last_Name varchar(20),email varchar(50),PhoneNo varchar(15),Password varchar(100),DOB date,AadharNo varchar(12) unique,Address varchar(150),BloodGrp varchar(5),Gender varchar(1));'
+const adminSchema = 'create table if not exists admin (admin_id int primary key auto_increment,admin_username varchar(20),admin_password varchar(100));'
+pool.query(userSchema,(error,result)=>{
+    if (error) {
+        console.log(error)
+    }
+    else{
+        console.log(result)
+    }
+})
+pool.query(adminSchema,(error,result)=>{
+    if (error) {
+        console.log(error)
+    }
+    else{
+        console.log(result)
+    }
+})
 module.exports = pool;
