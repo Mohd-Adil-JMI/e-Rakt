2a1cc5c3f98bd08875ad2c31a956d7f180331d53
Soban Farooq <sobanfarooq911@gmail.com>
2022-03-24 23:36:48 +0530

    resolved minor bugs

--- controllers/auth.js
+++ controllers/auth.js
@@ -71,7 +71,7 @@ exports.SignUp = (req, res) => {
       if (error) {
         console.log(error);
       } else {
-        return res.status(200).redirect('/SignUp');
+        return res.status(200).redirect('/Login');
       }
     });
   });
